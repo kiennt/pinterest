0427f495d3726b8e783a336a9facddb814c79e15
kiennt <trungkien2288@gmail.com>
2012-07-27 09:41:54 +0700

    Update pinterest ruby bot

--- pinterest.rb
+++ pinterest.rb
@@ -1,126 +1,144 @@
-#!/usr/bin/env ruby
 require 'httpclient'
 require 'nokogiri'
 require 'json'
 
 class PinterestBot
-    attr_reader :client, :boards
-
+    # Constructor
+    #
+    # username - String  
+    # password - String
     def initialize(email, password)
-        """ Constructor
-
-        @param :username String 
-        @param :password String
-        """
         @email = email
         @password = password
-        @boards = {}
         @client = HTTPClient.new
     end
     
+    # Private: Get value of cookie
+    # 
+    # res - Response object of HTTPClient Request 
+    # cookie_name - String name of cookie we want to get value 
+    #   
+    # Return a String value of cookie
     def _get_cookie_name(cookie_name)
-        """ Get value of cookie
-        
-        @param :res Response object of HTTPClient Request 
-        @param :cookie_name String name of cookie we want to get value 
-        @return String value of cookie
-        """
         @client.cookies.each do |cookie|
             return cookie.value if cookie.name == cookie_name
         end
     end
     
-    def get_csrftoken()
-        @client.cookies.each do |cookie|
-            return cookie.value if cookie.name == "csrftoken"
-        end
-    end
-
-    def login()
-        """ Login to pinterest
-
-        This function should be called after initialize
-        """
-        @client.get "https://pinterest.com/login/"
-        body =  {
-            :email => @email,
-            :password => @password,
-            :csrfmiddlewaretoken => self.get_csrftoken()
-        }
-        res = @client.post "https://pinterest.com/login/", body
-        self._get_list_board()
+    # Public: Get csrf token from cookies
+    # 
+    # Return a String value of csrf token
+    def csrftoken
+        _get_cookie_name "csrftoken"
     end
     
-    def _get_list_board()
+    # Public: Login to pinterest with authenticate information
+    # This function should be called after initialize
+    # 
+    # Example
+    #   bot = PinterestBot(email, password)
+    #   bot.login()
+    def login
+        @client.get "https://pinterest.com/login/"
+        body = {
+            :email => @email,
+            :password => @password,
+            :csrfmiddlewaretoken => csrftoken
+        }
+        res = @client.post "https://pinterest.com/login/", body
+    end
+   
+    # Public: Get username of this user
+    #
+    # Return a String    
+    def username 
+        return @username if @username
+        
+        puts "get username"
         res = @client.get "https://pinterest.com/me/"
-
         url = res.headers["Location"]
         url = url[0..-2] if url[-1] == '/'
         @username = url[(url.rindex('/') + 1)..-1]
+    end
 
-        res = @client.get "http://pinterest.com/#{@username}/"
+    # Public: Get list of board of this account
+    # 
+    # Return a Dictionary which 
+    #   key - String board name
+    #   value - String board id
+    def boards
+        return @boards if @boards
+        
+        puts "get boards"
+        @boards = {}
+        res = @client.get "http://pinterest.com/#{username}/"
         soup = Nokogiri::HTML(res.content)
+
         div_boards = soup.css("div[class=\"pin pinBoard\"]")
         div_boards.each do |board|
-            board_name = board.css("a")[0].content
+            name = board.css("a")[0].content
             board_id = board["id"][5..-1]
-            @boards[board_name] = board_id
+            @boards[name] = board_id
         end
+        @boards
     end
-
-    def get_board_id_for_name(category_name)
-        if not @boards.include?(category_name) then
-            self.create_new_category(category_name)
-        end
-        @boards[category_name]
-    end
-
-    def create_new_category(category_name)
-        """ Create new category
-
-        @param :category_name String
-        @return Dictionary object which contains
-            :url String ULR of new category
-            :status String :success is expected
-            :name String `category_name`
-            :id String ID of new category
-        """
-        puts "Create new category"
+    
+    # Public: Create new board
+    #
+    # name - String name of new board
+    #   
+    # Return a Dictionary object which contains
+    #   url - String ULR of new category
+    #   status - String :success is expected
+    #   name - String `category_name`
+    #   id - String ID of new category
+    def create_new_board(name)
         headers = {
-            "X-CSRFToken" => self.get_csrftoken(),
+            "X-CSRFToken" => csrftoken(),
             "X-Pinterest-Referrer" => "http://pinterest.com/",
             "X-Requested-With" => "XMLHttpRequest",
             "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11"
         }
         body = {
-            :name => category_name,
+            :name => name,
             :pass_category => true,
         }  
         res = @client.post "https://pinterest.com/board/create/", body, headers
-        @boards[category_name] = JSON.parse(res.content)[:id]
+        JSON.parse(res.content)
     end
-    
 
-    def upload(category, description, photo_path)
-        """ Upload photo to pinterest
+    # Public: Get id for board name
+    # 
+    # name - String name of board     
+    # 
+    # Return String id of board
+    def get_board_id_for_name(name)
+        if not boards.include?(name) then
+            res = create_new_board(name)
+            board_id = res[:id]
+            boards[name] = board_id
+        end
 
-        @param :data Dictionary which contains
-            :description String
-            :photo_path String path of photo in machine
-            :category Category name you want to put new photo in
-        """
-        index = photo_path.rindex('/')
-        if index
-            filename = photo_path[(index + 1)..-1]
-        else
-            filename = photo_path
-        end 
+        boards[name]
+    end
 
-        File.open(photo_path) do |file|
+    # Public: Upload photo to pinterest
+    #
+    # data - Dictionary which contains
+    #   :caption - String caption you want to put in image
+    #   :path - String path of photo in machine
+    #   :board - String name of board you want to put new photo in
+    #
+    # Return Dictionary which contains
+    #   :status - String "success" expected
+    #   :url - String with format "/pin/<pin_id>/"
+    #   :message - String "posted." expected
+    def create_pin(data)
+        File.open(data[:path]) do |file|
             body = {
-                "csrfmiddlewaretoken" => self.get_csrftoken(),
-                "board" => self.get_board_id_for_name(category),
-                "details" => description,
+                "csrfmiddlewaretoken" => csrftoken,
+                "board" => get_board_id_for_name(data[:board]),
+                "details" => data[:caption],
                 "link" => "",
                 "img_url" => "",
                 "tags" => "",
@@ -129,15 +147,19 @@ class PinterestBot
                 "img" => file
             }
             res = @client.post "http://pinterest.com/pin/create/", body
-            return res
+            res.content
         end
     end
+
 end
 
-def test()
-    bot = PinterestBot.new('knt.code@gmail.com', '@123456@')
-    bot.login()
-    res = bot.upload("knt-test", "Description", "/DATA/Dropbox/MacImage/IMG_0167.JPG")
+bot = PinterestBot.new('knt.code@gmail.com', '@123456@')
+bot.login
+10.times do |i|
+    res = bot.create_pin({
+        :board => "knt-test2", 
+        :caption => "Caption #{i}", 
+        :path => "/DATA/Dropbox/MacImage/IMG_0167.JPG"
+    })
+    puts res
 end
-
-test()
